7045d78083c6f54a17d0cca7a24cbf4928558aee
lonelytransistor <christopher.815.0@gmail.com>
2022-02-20 21:22:58 +0100

    Fixed wrong helper script

--- root/athenaHelper.sh
+++ root/athenaHelper.sh
@@ -7,6 +7,9 @@ if [ "$1" == "--adjust-epd" ]; then
     done
 fi
 if [ "$1" == "--pre-set-environment" ]; then
-    cmd=$(grep '#' /etc/environments.d/$2 | tr '#' ' ; ')
-    bash -c "echo Executing reconfig for $2 now. ${cmd}"
+    grep '#' /etc/environments.d/$2 > /tmp/athenaHelper.tmp0
+    grep '#' /etc/environments.d/$2 | sed 's|^#||g' > /tmp/athenaHelper.tmp1
+    bash /tmp/athenaHelper.tmp1
+    cat /tmp/athenaHelper.tmp0 >> /etc/environments.d/$2
+    rm /tmp/athenaHelper.tmp0 /tmp/athenaHelper.tmp1
 fi
